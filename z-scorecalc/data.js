8b988d08f5cea7a152acabaa7e45b70eb71cfd11
habibip2 <134698632+habibip2@users.noreply.github.com>
2024-09-30 17:40:52 +0700

    Add files via upload

---
+++ z-scorecalc/data.js
@@ -0,0 +1,44 @@
+const data = [
+    {
+        jenisKelamin: 'laki-laki',
+        usia: [
+            { bulan: 1, bb_u: 2.5, tb_u: 50, pb_u: 47, lika_u: 47, lila_u: 46 },
+            { bulan: 2, bb_u: 3.5, tb_u: 55, pb_u: 50, lika_u: 47, lila_u: 46 },
+            { bulan: 3, bb_u: 4.5, tb_u: 60, pb_u: 53, lika_u: 47, lila_u: 46 },
+            { bulan: 4, bb_u: 5.5, tb_u: 65, pb_u: 55, lika_u: 47, lila_u: 46 },
+            // Tambahkan data sampai 60 bulan
+        ],
+        z1: [
+            { bulan: 1, bb_u: 2.5, tb_u: 50, pb_u: 47, lika_u: 47, lila_u: 46 },
+            { bulan: 2, bb_u: 3.5, tb_u: 55, pb_u: 50, lika_u: 47, lila_u: 46 },
+            { bulan: 3, bb_u: 4.5, tb_u: 60, pb_u: 53, lika_u: 47, lila_u: 46 },
+            { bulan: 4, bb_u: 5.5, tb_u: 65, pb_u: 55, lika_u: 47, lila_u: 46 },
+            // Tambahkan data sampai 60 bulan
+        ],
+        z0: [
+            { bulan: 1, bb_u: 2.5, tb_u: 50, pb_u: 47, lika_u: 47, lila_u: 46 },
+            { bulan: 2, bb_u: 3.5, tb_u: 55, pb_u: 50, lika_u: 47, lila_u: 46 },
+            { bulan: 3, bb_u: 4.5, tb_u: 60, pb_u: 53, lika_u: 47, lila_u: 46 },
+            { bulan: 4, bb_u: 5.5, tb_u: 65, pb_u: 55, lika_u: 47, lila_u: 46 },
+            // Tambahkan data sampai 60 bulan
+        ]
+    },
+    {
+        jenisKelamin: 'perempuan',
+        usia: [
+            { bulan: 1, bb_u: 2.4, tb_u: 49, pb_u: 46, lika_u: 47, lila_u: 46 },
+            { bulan: 2, bb_u: 3.4, tb_u: 54, pb_u: 49, lika_u: 47, lila_u: 46 },
+            { bulan: 3, bb_u: 4.4, tb_u: 59, pb_u: 52, lika_u: 47, lila_u: 46 },
+            { bulan: 4, bb_u: 5.4, tb_u: 64, pb_u:54 , lika_u: 47, lila_u :46},
+            // Tambahkan data sampai ke bulan ke-60
+        ],
+        z1:[ { bulan: 1, bb_u: 2.5, tb_u: 50, pb_u: 47, lika_u: 47, lila_u: 46 },
+            { bulan: 2, bb_u: 3.5, tb_u: 55, pb_u: 50, lika_u: 47, lila_u: 46 },
+            { bulan: 3, bb_u: 4.5, tb_u: 60, pb_u: 53, lika_u: 47, lila_u: 46 },
+            { bulan: 4, bb_u: 5.5, tb_u: 65, pb_u: 55, lika_u: 47, lila_u: 46 }, ],
+        z0:[ { bulan: 1, bb_u: 2.5, tb_u: 42, pb_u: 47, lika_u: 47, lila_u: 46 },
+            { bulan: 2, bb_u: 3.5, tb_u: 55, pb_u: 50, lika_u: 47, lila_u: 46 },
+            { bulan: 3, bb_u: 4.5, tb_u: 60, pb_u: 53, lika_u: 47, lila_u: 46 },
+            { bulan: 4, bb_u: 5.5, tb_u: 65, pb_u: 55, lika_u: 47, lila_u: 46 },]
+    }
+];
