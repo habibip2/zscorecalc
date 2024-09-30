a2a1d46eb42cc8b82501b17ee13e9af645decfa9
habibip2 <134698632+habibip2@users.noreply.github.com>
2024-10-01 05:01:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -29,5 +29,41 @@ document.getElementById('hitung').addEventListener('click', function() {
     }
 document.getElementById('hasil').innerText = ''; 
     document.getElementById('hasil').innerText += `${zScore.toFixed(2)} \n`;
-});
+
+    // Menambahkan komentar sesuai dengan indikator dan Z-Score
+            let comment = '';
+            if (indikator === 'bb_u') {
+                if (zScore < -3) {
+                    comment = 'BBSK (Berat Badan Sangat Kurang)';
+                } else if (zScore >= -3 && zScore < -2) {
+                    comment = 'BBK (Berat Badan Kurang)';
+                } else if (zScore >= -2 && zScore <= 1) {
+                    comment = 'Normal';
+                } else if (zScore > 1) {
+                    comment = 'Resiko BB Lebih';
+                }
+            } else if (indikator === 'pb_u' || indikator === 'tb_u') {
+                if (zScore < -3) {
+                    comment = 'Sangat Pendek';
+                } else if (zScore >= -3 && zScore < -2) {
+                    comment = 'Pendek';
+                } else if (zScore >= -2 && zScore <= 3) {
+                    comment = 'Normal';
+                } else if (zScore > 3) {
+                    comment = 'Tinggi';
+                }
+            } else if (indikator === 'lika_u') {
+                if (zScore < -2) {
+                    comment = 'Mikrosefalus';
+                } else if (zScore >= -2 && zScore <= 2) {
+                    comment = 'Normal';
+                } else if (zScore > 2) {
+                    comment = 'Makrosefalus';
+                }
+            }
+
+            // Menampilkan comment di bawah hasil Z-Score
+            resultArea.innerText = comment;
+        });
+
                                             
